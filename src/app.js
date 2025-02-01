20a33029acf64a0828435caec4421bba0198f06c
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2025-02-01 19:42:29 +0530

    multiple route hanlder with the same routes.

--- src/app.js
+++ src/app.js
@@ -2,37 +2,33 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Himanshu", lastName: "Sharma" });
-});
+app.get(
+  "/user",
+  [
+    (req, res, next) => {
+      console.log("Handling the first route handler!");
+      // res.send("1st response");
+      next();
+    },
+    (req, res, next) => {
+      // res.send("2nd response!");
+      console.log("Handling the second route handler!");
+      next();
+    },
+  ],
+  [
+    (req, res, next) => {
+      console.log("Handling the third route handler!");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Handling the fourth route handler!");
+      res.send("4th response");
+      next();
+    },
+  ]
+);
 
-app.post("/user", (req, res) => {
-  // Saving the Data which is coming from req.body in to the Database.
-  res.send("Data is Sucessfully saved in to the Database!");
-});
-
-app.patch("/user", (req, res) => {
-  // Updating Data into the Database.
-  res.send("Data is Sucessfully updated in to the Database!");
-});
-
-app.delete("/user", (req, res) => {
-  // Delete the Data from Database.
-  res.send("Data is Deleted Sucessfully from the Database!");
-});
-//  Query params:-
-// app.use("/users", (req, res) => {
-//   console.log(req.query);
-//   res.send("Hello!");
-// });
-
-// dynamic routes:-
-
-app.use("/users/:firstName/:lastName/:dob", (req, res) => {
-  console.log(req.params);
-  res.send("dynamic route");
-});
-
-app.listen(8085, () => {
-  console.log("Server is Sucessfully listen on 8085!");
+app.listen("7777", () => {
+  console.log("Server is successfully listening on PORT:7777");
 });
