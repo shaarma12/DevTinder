5718d5b83d3a9d397de302efe1079f352f980402
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2025-02-16 18:50:01 +0530

    user connection API

--- src/models/connection.js
+++ src/models/connection.js
@@ -11,6 +11,7 @@ const connectionRequestSchema = new mongoose.Schema(
     toUserId: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
+      ref: User,
     },
     status: {
       type: String,
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -25,4 +25,34 @@ router.get("/request", authUser, async (req, res) => {
   }
 });
 
+router.get("/connection", authUser, async (req, res) => {
+  try {
+    const connections = await ConnectionRequest.find({
+      $or: [
+        { status: "accepted", toUserId: req.userId },
+        { status: "accepted", fromUserId: req.userId },
+      ],
+    })
+      .populate("fromUserId", "firstName lastName age photoURL about skills")
+      .populate("toUserId", "firstName lastName age photoURL about skills");
+
+    if (connections.length === 0) {
+      return res.status(404).json({ message: "No connection found!" });
+    }
+
+    const data = connections.map((row) => {
+      if (row.fromUserId._id.toString() === req.userId.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+
+    res.json({ message: "Getting all the connections", data: data });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: "Something went wrong", error: err.message });
+  }
+});
+
 module.exports = router;
